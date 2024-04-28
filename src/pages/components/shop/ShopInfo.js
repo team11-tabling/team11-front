bce641284e95a7a56cf94e9ebcebd9cfdf4cded8
Hako99 <154326391+Hako99@users.noreply.github.com>
2024-04-28 16:26:18 +0900

    Update ShopInfo.js

--- src/pages/components/shop/ShopInfo.js
+++ src/pages/components/shop/ShopInfo.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-//import {EventSourcePolyfill} from "event-source-polyfill";
+import {EventSourcePolyfill} from "event-source-polyfill";
 
 function ShopInfo(props) {
   const [shop, setShop] = useState(null);
-  const [waitingNum, setWaitingNum] = useState();
+  const [waitingNum, setWaitingNum] = useState('');
   const { shopId } = props;
   const token = localStorage.getItem('Authorization');
 
@@ -14,7 +14,7 @@ function ShopInfo(props) {
         Authorization: `${token}`
       }
     };
-    fetch(`https://load.p-hako.com/api/shops/${shopId}`, requestOptions)
+    fetch(`http://localhost:8080/api/shops/${shopId}`, requestOptions)
     .then(response => response.json())
     .then(data => {
       const shopData = data.data || null; // 데이터가 없을 경우를 대비하여 null로 설정
@@ -23,15 +23,27 @@ function ShopInfo(props) {
     .catch(error => {
       console.error('Error fetching data: ', error);
     });
-    // SSE 연결 설정
-    const eventSource = new EventSource(`https://load.p-hako.com/api/shops/waiting-info/${shopId}`);
+
+    //SSE 연결 설정
+    const eventSource = new EventSourcePolyfill(`http://localhost:8080/api/shops/waiting-info/${shopId}`, {
+      headers: {
+        Authorization: token,
+      },
+      heartbeatTimeout: 86400000,
+      withCredentials : true
+    });
     eventSource.onmessage = event => {
       console.log(eventSource)
       console.log("대기인 수 " + event.data)
       setWaitingNum(event.data);  // 대기 인원 수 업데이트
     };
+    eventSource.onerror = function(error) {
+      console.error('EventSource failed:', error);
+      eventSource.close();
+    };
 
     return () => eventSource.close();  // 컴포넌트 언마운트 시 SSE 연결 해제
+
   }, [shopId, token]); // shopId와 token을 의존성 배열에 추가하여, 해당 값이 변경될 때마다 useEffect가 실행되도록 함
   if (!shop) {
     return <div>Loading...</div>;
